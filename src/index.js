edb746c321464c536cb8436222ca5b4e73f89776
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-02-19 11:17:56 -0600

    Reducers is set up~

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { createStore, applyMiddleware } from 'redux'
 import "./index.css";
 import App from "./App";
+import { Provider } from 'react-redux'
+import { reducer } from './reducers'
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
+//Creating My Store Here -------------------------------------------------------
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    //Wrapping App and it's children with the Provider, passing in my store! ---------------------
+    <Provider store={store}>
+    <App />
+    </Provider>, 
     rootElement
 );
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,62 @@
 
+//Initial State Here --------------------------------------------------
 export const initialState = {
+        smurfs:[],
+        isLoading: false,
+        error: '',
 }
 
-const reducer = ()=>{
+export const reducer = (state = initialState, action) => {
+
+    //Switch Statement Here --------------------------------------------------
+    switch(action.type) {
+        case("FETCH_SMURF_LOADING"): //This starts the fetch! 
+            return({
+                ...state,
+                isLoading: true
+            });
+        case("FETCH_SMURF_SUCCESS"): //Sets up state upon a successful fetch!
+            return({
+                smurfs:[],
+                isLoading: false
+            });
+        case("FETCH_SMURF_FAIL"): //Sets up state upon a failed fetch!
+            return({
+                ...state,
+                error: action.payload,
+                isLoading: false
+            });
+        case("ADD_SMURF"): //Sets up state when a new smurf is added in!
+            return({
+                ...state,
+                isLoading: false,
+                error: '',
+                smurfs: [...state.smurfs, {id: Date.now(), ...action.payload}]
+            });
+        case("SET_ERROR")://Sets up state when there is an error in the form!
+            return ({
+                ...state,
+                isLoading: false,
+                error: action.payload
+            })
+        default: //Returns the initial state if none of the above cases are true!
+            return state;
+            
+    }
+
 }
 
-export default reducer;
+
 
 //Task List:
 //1. Adds the following state values into the initialState:
-//  - an array of smurfs
-//  - a boolean indicating if the app is loading
-//  - a string indicating a possible error message
+//  - an array of smurfs ✔️
+//  - a boolean indicating if the app is loading ✔️
+//  - a string indicating a possible error message ✔️
 
-//2. Add in the arguments needed to complete a standard reducer function.
-//3. Add in a reducer case to accomidate the start of a smurf fetch.
-//4. Add in a reducer case to accomidate the successful smurf api fetch.
-//5. Add in a reducer cases to accomidate the failed smurf api fetch.
-//6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
-//7. Add in a reducer case that adds in a value to the error message.
+//2. Add in the arguments needed to complete a standard reducer function.✔️
+//3. Add in a reducer case to accomidate the start of a smurf fetch.✔️
+//4. Add in a reducer case to accomidate the successful smurf api fetch.✔️
+//5. Add in a reducer cases to accomidate the failed smurf api fetch. ✔️
+//6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list. ✔️
+//7. Add in a reducer case that adds in a value to the error message. ✔️
