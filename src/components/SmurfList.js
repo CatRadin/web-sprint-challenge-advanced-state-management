0bf6a8ddeae2dc9557e6b523411b1594b130f287
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-02-19 13:29:08 -0600

    Finished SmurfList

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import Smurf from './Smurf';
+import { connect } from 'react-redux'
 
- const SmurfList = ()=> {
-    const isLoading = false;
-    const testSmurf = {
-        id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-        name:'Poppa Smurf',
-        position:'Village Leader',
-        nickname: 'Pops',
-        description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
-    }
+ const SmurfList = (props)=> {
+    const { smurfs, isLoading } = props;
 
+    //Loading Message when it's fetching the smurfs from the array
     if (isLoading) {
         return <h1>Loading...</h1>;
     }
 
+    //Mapping over the smurfs in the array and returns them as individualized smurfs--------------
     return(<div className="listContainer">
-        <Smurf smurf={testSmurf}/>
+        {
+            smurfs.map((smurf) => {
+                return <Smurf smurf={smurf} key={smurf.id} />
+            })
+        }
     </div>);
 }
 
-export default SmurfList;
+//Map State To Props The State I am passing into this component using Redux --------------------------------
+const mapStateToProps = (state) => {
+    return {
+        smurfs: state.smurfs,
+        isLoading: state.isLoading
+    }
+}
+
+export default connect(mapStateToProps)(SmurfList);
 
 //Task List:
-//1. Connect the smurfs and loading state values to the SmurfList component.
-//2. Replace the single Smurf component instance with a map return a Smurf component for each entry in the smurfs list.
-//3. Replace the static isLoading variable with the state loading variable.
+//1. Connect the smurfs and loading state values to the SmurfList component. ✔️
+//2. Replace the single Smurf component instance with a map return a Smurf component for each entry in the smurfs list. ✔️
+//3. Replace the static isLoading variable with the state loading variable.✔️
